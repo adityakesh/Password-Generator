9c1482975fdf8b5857f861c8720362ab758a127f
adityakesh <adityababukesharwani2@gmail.com>
2024-09-07 17:07:48 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import { useState } from "react";
+import "./App.css";
+import { UC, LC, NC, SC } from "./data/PassChar";
 
 function App() {
+  let [uppercase,setUppercase]=useState(false)
+  let [lowercase,setLowercase]=useState(false)
+  let [numbers,setNumbers]=useState(false)
+  let [symbols,setSymbols]=useState(false)
+  let [passlength,setPasslength]=useState()
+  let [fPass,setPass]=useState('')
+
+  let createPassword=()=>{
+    let finalPass=''
+    let charSet = ''
+    if(uppercase || lowercase || numbers || symbols){
+      if(uppercase) charSet+=UC;
+      if(lowercase) charSet+=LC;
+      if(numbers) charSet+=NC;
+      if(symbols) charSet+=SC;
+      for(let i = 0;i<passlength;i++){
+        finalPass += charSet.charAt(Math.floor(Math.random()*charSet.length))
+      }
+      setPass(finalPass)
+
+    }else {
+      alert('Please select atleast one CheckBox!....')
+    }
+  }
+
+  let copyPass=()=>{
+    navigator.clipboard.writeText(fPass)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="passwordBox">
+        <h2>Password Generator</h2>
+        <div className="passwordBoxin">
+          <input type="text" value={fPass} readOnly /> <button 
+          onClick={copyPass}>Copy</button>
+        </div>
+        <div className="passLength">
+          <lable>Password Length</lable>
+          <input type="number" max={20} min={4} value={passlength} 
+          onChange={(event)=>setPasslength(event.target.value)}/>
+        </div>
+        <div className="passLength">
+          <lable>Include uppercase letters</lable>
+          <input type="checkbox" checked={uppercase} 
+          onChange={()=>setUppercase(!uppercase)}/>
+        </div>
+        <div className="passLength">
+          <lable>Include lowercase letters</lable>
+          <input type="checkbox" checked={lowercase} 
+          onChange={()=>setLowercase(!lowercase)}/>
+        </div>
+        <div className="passLength">
+          <lable>Include Numbers</lable>
+          <input type="checkbox" checked={numbers} 
+          onChange={()=>setNumbers(!numbers)}/>
+        </div>
+        <div className="passLength">
+          <lable>Include Symbols</lable>
+          <input type="checkbox" checked={symbols} 
+          onChange={()=>setSymbols(!symbols)}/>
+        </div>
+        <button className='btn' onClick={createPassword}>
+          Generate Password
+        </button>
+      </div>
+    </>
   );
 }
 
